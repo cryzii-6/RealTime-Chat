0f27bb59d4354834233c68fa92d2b389ad92582a
Ynnyannah Cruz <100091343+cryzii-6@users.noreply.github.com>
2024-12-17 23:18:57 +0800

    Add files via upload

---
+++ website/__init__.py
@@ -0,0 +1,51 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from .extensions import socketio
+import json
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # Absolute path to the config file
+    config_path = 'chatapp/config.json'  # Directly reference from the root folder
+
+    # Debugging: print the absolute path to verify
+    print("Looking for config file at:", os.path.abspath(config_path))
+
+    # Open and load the config.json
+    with open(config_path, 'r') as config_file:
+        config = json.load(config_file)
+
+    app.config['SECRET_KEY'] = config['SECRET_KEY']
+    app.config['UPLOAD_FOLDER'] = config['UPLOAD_FOLDER']
+    app.config['SQLALCHEMY_DATABASE_URI'] = config['SQLALCHEMY_DATABASE_URI']
+    db.init_app(app)
+
+    with app.app_context():
+        from .models import User, Message, Room
+        from .views import views
+        from .auth import auth
+        from .functions import get_admins
+
+        app.register_blueprint(views, url_prefix='/')
+        app.register_blueprint(auth, url_prefix='/')
+
+        socketio.init_app(app)
+
+        login_manager = LoginManager()
+        login_manager.login_view = 'auth.login'
+        login_manager.init_app(app)
+
+        @login_manager.user_loader
+        def load_user(id):
+            return User.query.get(str(id))
+
+        db.create_all()
+        
+    return app
